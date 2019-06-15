ef3679dfd106550125f68cbad58843be0f486fd8
Naiviv <46464295+Naiviv@users.noreply.github.com>
2019-06-14 23:49:57 -0700

    Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
-console.log('OK')
+console.log('OK');
+
+// Global
+
+$('.tabs').tabslet();
+
+let date = new Date();
+let currentYear = date.getFullYear(); 
+
+console.log( currentYear );
+console.log( date);
+
+$('footer p span').text( currentYear );
+
+$('#one , #two , #three , #four , #five , #six , #seven , #eight , #nine').Chocolat();
+
+
+
+
+
+
+//Portfolio
+
+// $('.portfolio').Chocolat({
+
+//    separator2: '--',
+//    fullScreen: true,
+//    enableZoom: false,
+// });
